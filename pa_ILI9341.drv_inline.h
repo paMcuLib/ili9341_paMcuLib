1052b138e18322536f2aff253b03f02e42ef8da8
ActivePeter <1020401660@qq.com>
2021-06-15 13:56:15 +0800

    是

--- pa_ILI9341.drv_inline.h
+++ pa_ILI9341.drv_inline.h
@@ -1,13 +1,29 @@
 #include "pa_ILI9341.drv_inline.inc.h"
 
+inline void initGpio()
+{
+    _G_paGPIO.init(
+        N_paGPIO::Mode_Output,
+        Conf_ILI9341_CS);
+    _G_paGPIO.init(
+        N_paGPIO::Mode_Output,
+        Conf_ILI9341_DC);
+    _G_paGPIO.init(
+        N_paGPIO::Mode_Output,
+        Conf_ILI9341_RST);
+}
+
 inline void setCS(uint8_t state)
 {
+    _G_paGPIO.write(Conf_ILI9341_CS, state);
 }
 inline void setDC(uint8_t state)
 {
+    _G_paGPIO.write(Conf_ILI9341_DC, state);
 }
 inline void setRST(uint8_t state)
 {
+    _G_paGPIO.write(Conf_ILI9341_RST, state);
 }
 inline void delayMs(uint32_t ms)
 {
--- pa_ILI9341.drv_inline.inc.h
+++ pa_ILI9341.drv_inline.inc.h
@@ -1,5 +1,6 @@
 #include "paBase/_ForMod.h"
 #include "paCoreHead/paBase.h"
 #include "paCoreHead/paSPI.h"
+#include "paCoreHead/paGPIO.h"
 
 #include "Conf_ILI9341.h"
